e180b75787b4363736591159dcb7a9985d574adc
Liwei <lifvic.xu@gmail.com>
2016-12-28 07:37:34 +0000

    Add initial markup for messages. Add some initial styling. Some code clean-up.

--- canvas.js
+++ canvas.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var pug = require("pug");
 var path = require("path");
-var io = require('socket.io');
+var io = require("socket.io");
 
 var app = express()
-  , server = require('http').createServer(app)
+  , server = require("http").createServer(app)
   , io = io.listen(server);
 
 app.set("views", path.join(__dirname, "templates"));
@@ -12,8 +12,8 @@ app.set("views", path.join(__dirname, "templates"));
 // Static files under public/ directory.
 app.use(express.static("public"));
 
-app.get('/', function(req, res){
-  res.render('canvas.pug');
+app.get("/", function(req, res){
+  res.render("canvas.pug");
 });
 
 var user_manager = {
@@ -29,7 +29,7 @@ var user_manager = {
     }
   },
   broadcast: function() {
-    io.emit('users', this.active_users);
+    io.emit("users", this.active_users);
   }
 };
 
@@ -41,48 +41,56 @@ function getRandomColor() {
   var r = getRandomArbitrary(0, 255);
   var g = getRandomArbitrary(0, 255);
   var b = getRandomArbitrary(0, 255);
-  var color = "rgb("+ r+","+g+","+b+")";
+  var color = "rgb("+ r + "," + g + "," + b + ")";
   
   return color;
 }
 
 var user_counter = 0;
 var message_counter = 0;
-io.on('connection', function(socket){
-  socket.on('join', function(user_name){
+io.on("connection", function(socket){
+  socket.on("join", function(user_name){
     var user_id = user_counter++;
     var color = getRandomColor();
     
     user_manager.user_joined(user_id, user_name);
 
-    socket.on('disconnect', function() {
+    socket.on("disconnect", function() {
       user_manager.user_left(user_id)
     });
     
-    socket.on('message', function(message){
-      io.emit('message', {text: message,
-                          key: message_counter});
+    socket.on("message", function(message){
+      io.emit("message", {text: message,
+                          user_id: user_id,
+                          user_name: user_name,
+                          key: message_counter,
+                          timedate: "8:01 A.M. Dec 24 2015"
+      });
       ++message_counter;
     });
     
-    socket.on('new_canvas', function(){
-      io.emit('new_canvas', "canvas_" + message_counter);
+    socket.on("new_canvas", function(){
+      io.emit("new_canvas", {canvas_id: "canvas_" + message_counter,
+                             user_id: user_id,
+                             user_name: user_name,
+                             timedate: "8:01 A.M. Dec 24 2015"
+      });
       ++message_counter;
     });
     
-    socket.on('click', function(pos){
-      io.emit('click', {"canvasId": pos.canvasId,
-                        "x": pos.x,
-                        "y": pos.y,
-                        "user_id": user_id,
-                        "color": color});
+    socket.on("click", function(pos){
+      io.emit("click", {canvasId: pos.canvasId,
+                        x: pos.x,
+                        y: pos.y,
+                        user_id: user_id,
+                        color: color});
     });
     
-    socket.on('mousemove', function(move){
-      io.emit('mousepaint', {"canvasId": move.canvasId,
-                             "x": move.x,
-                             "y": move.y,
-                             "user_id": user_id});
+    socket.on("mousemove", function(move){
+      io.emit("mousepaint", {canvasId: move.canvasId,
+                             x: move.x,
+                             y: move.y,
+                             user_id: user_id});
     });
   });
 });
--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -7,21 +7,23 @@ var socket = io();
 
 var ChatHeader = React.createClass({
   getInitialState: function() {
-    socket.on('users', this.onSocketUsers);
-    return {users: []};
+    socket.on("users", this.onSocketUsers);
+    return {users: {}};
   },
   onSocketUsers(users) {
-    this.setState({'users': users});
+    this.setState({users: users});
   },
   render: function() {
     var users = [];
     for (var id in this.state.users) {
       var user_name = this.state.users[id];
       
-      users.push(<span key={id} className='user'>{user_name}</span>);
+      users.push(<span key={id} className="header_user">{user_name}</span>);
     }
-    var label = users.length == 1 ? "user" : "users";
-    return (<div>Online({users.length} {label}): {users}</div>);
+    return (<div>
+              <div id="online_header">Currently Online</div>
+              <div id="online_users">{users}</div>
+            </div>);
   }
 });
 
@@ -29,22 +31,22 @@ var ChatCanvas = React.createClass({
   funcClick: function(e) {
     this.is_drawing = !this.is_drawing;
     
-    var click = {"canvasId": this.props.canvasId,
-                 "x": e.offsetX,
-                 "y": e.offsetY};
+    var click = {canvasId: this.props.canvasId,
+                 x: e.offsetX,
+                 y: e.offsetY};
     socket.emit("click", click);
   },
   render: function() {
     return (
-      <div><canvas width='400' height='300'
+      <div><canvas width="400" height="300"
               id={this.props.canvasId}></canvas></div>
       );
   },
   onMouseMove: function(e) {
     if (this.is_drawing) {
-      var data = {"canvasId": this.props.canvasId,
-                  "x": e.offsetX,
-                  "y": e.offsetY};
+      var data = {canvasId: this.props.canvasId,
+                  x: e.offsetX,
+                  y: e.offsetY};
       socket.emit("mousemove", data);
     }
   },
@@ -52,15 +54,15 @@ var ChatCanvas = React.createClass({
     if (this.canvas) return;
     
     this.canvas = document.getElementById(this.props.canvasId);
-    this.drawing = {ctx: this.canvas.getContext('2d')};
+    this.drawing = {ctx: this.canvas.getContext("2d")};
     this.is_drawing = false;
     this.last_position = {};
     this.color_map = {};
     
-    socket.on('click', this.onSocketClick);
-    socket.on('mousepaint', this.onMousePaint);
-    $(this.canvas).on('click', this.funcClick);
-    $(this.canvas).on('mousemove', this.onMouseMove);
+    socket.on("click", this.onSocketClick);
+    socket.on("mousepaint", this.onMousePaint);
+    $(this.canvas).on("click", this.funcClick);
+    $(this.canvas).on("mousemove", this.onMouseMove);
   },
   onSocketClick: function(data) {
     if (data.canvasId != this.props.canvasId) return;
@@ -70,15 +72,15 @@ var ChatCanvas = React.createClass({
       delete this.last_position[data.user_id];
     } else {
       // start point.
-      this.last_position[data.user_id] = { "x": data.x, 
-                                           "y": data.y };
+      this.last_position[data.user_id] = {x: data.x, 
+                                          y: data.y};
       this.color_map[data.user_id] = data.color;
     }
   },
   onMousePaint: function(data) {
     if (data.canvasId != this.props.canvasId) return;
     
-    var new_point = {"x": data.x, "y": data.y};
+    var new_point = {x: data.x, y: data.y};
     this.drawLine(this.last_position[data.user_id], new_point, this.color_map[data.user_id]);
     
     this.last_position[data.user_id] = new_point;
@@ -96,8 +98,8 @@ var ChatCanvas = React.createClass({
 
 var ChatMessages = React.createClass({
   getInitialState: function() {
-    socket.on('message', this.onSocketMessage);
-    socket.on('new_canvas', this.newCanvas);
+    socket.on("message", this.onSocketMessage);
+    socket.on("new_canvas", this.newCanvas);
     return {messages: []};
   },
   onSocketMessage: function(message) {
@@ -105,9 +107,9 @@ var ChatMessages = React.createClass({
     message_list.push(message);
     this.setState({messages: message_list});
   },
-  newCanvas: function(canvas_id) {
+  newCanvas: function(canvas_data) {
     var message_list = this.state.messages;
-    message_list.push({'canvas': canvas_id});
+    message_list.push(canvas_data);
     this.setState({messages: message_list});
   },
   render: function() {
@@ -116,11 +118,23 @@ var ChatMessages = React.createClass({
     for (var i = 0; i < this.state.messages.length; i++) {
       var message = this.state.messages[i];
       
-      if ('text' in message) {
-        messages.push(<div key={message.key} className='message'>{message.text}</div>);
-      } else if ('canvas' in message) {
-        messages.push(<ChatCanvas key={message.canvas} canvasId={message.canvas}/>);
+      var content;
+      if ("text" in message) {
+        content = <div className="message_content">{message.text}</div>;
+      } else if ("canvas_id" in message) {
+        content = <ChatCanvas key={message.canvas} canvasId={message.canvas_id}/>;
       }
+
+      messages.push(
+        <article key={message.key} className="message">
+          <div className="message_user_photo"><img src="http://placehold.it/80x60"/></div>
+          <div className="message_data">
+            <p className="message_user_name">{message.user_name}</p>
+            <p className="message_time">{message.timedate}</p>
+            {content}
+          </div>
+          <hr/>
+        </article>);
     }
     
     return (
@@ -130,7 +144,7 @@ var ChatMessages = React.createClass({
     );
   },
   componentDidUpdate: function() {
-    $('#chatmessages').scrollTop(function() {return this.scrollHeight});
+    $("#chatmessages").scrollTop(function() {return this.scrollHeight});
   }
 });
 
@@ -145,31 +159,31 @@ var ChatInput = React.createClass({
     );
   },
   onKeyPress: function(e) {
-    if (e.key === 'Enter' && e.target.value.length > 0) {
-      socket.emit('message', e.target.value);
-      e.target.value = '';
+    if (e.key === "Enter" && e.target.value.length > 0) {
+      socket.emit("message", e.target.value);
+      e.target.value = "";
     }
   },
   newCanvas: function() {
-    socket.emit('new_canvas');
+    socket.emit("new_canvas");
   }
 });
 
 ReactDOM.render(
   <ChatHeader />,
-  document.getElementById('chatheader')
+  document.getElementById("chatheader")
 );
 ReactDOM.render(
   <ChatMessages />,
-  document.getElementById('chatmessages')
+  document.getElementById("chatmessages")
 );
 ReactDOM.render(
   <ChatInput />,
-  document.getElementById('chatinput')
+  document.getElementById("chatinput")
 );
 
 // TODO: replace this with better intro screen.
 var person = prompt("Please enter your name", "");
 if (person != null) {;
-  socket.emit('join', person);
+  socket.emit("join", person);
 }
